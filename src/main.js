a63ded007ff2d0f59d964b53d0390de45f732eed
xiaokai666 <759502091@qq.com>
2019-10-31 15:48:01 +0800

    项目目录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/routen'
 Vue.config.productionTip = false
-
+// Element是个插件，包含了很多组件
+Vue.use(Element)
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
